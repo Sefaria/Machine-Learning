d85aec6fa28982978f1418c45c964775b5c95bbf
yon <yonadav.le@gmail.com>
2023-01-12 01:09:40 +0200

    moved rulebased_ner to machine_learning repo, train en model on first 10 pages of each bavli masechet

---
+++ torah_ner/scripts/trim_mentions.py
@@ -0,0 +1,55 @@
+import django, json, srsly, csv
+
+django.setup()
+from sefaria.model import *
+from tqdm import tqdm
+import re
+
+#
+#
+# {
+#     "text": "אמר רב הונא שכיב מרע שהקדיש כל נכסיו ואמר מנה לפלוני בידי נאמן חזקה אין אדם עושה קנוניא על הקדש",
+#     "spans": [
+#       {
+#         "start": 4,
+#         "end": 11,
+#         "token_start": 1,
+#         "token_end": 2,
+#         "label": "Person"
+#       }
+#     ],
+#     "meta": {
+#       "ref": "Bava Batra 174b:19",
+#       "version_title": "William Davidson Edition - Aramaic",
+#       "language": "he"
+#     }
+#   }
+
+
+
+if __name__ == "__main__":
+    print("hello world")
+    # stats
+
+    with open('aggregated_he.json') as f:
+        mentions = json.load(f)
+
+    trimmed_training = []
+    test = []
+
+
+    for mention in tqdm(mentions, desc="trimming mentions"):
+        page_number = re.search(r"\d+", mention['meta']['ref'])
+        if int(page_number.group(0)) < 10:
+            trimmed_training.append(mention)
+        else:
+            test.append(mention)
+
+    #aggregated_list = list(aggregated_dict.values())
+
+    with open("trimmed_training_he.json", "w") as f:
+        # Write the list of dictionaries to the file as JSON
+        json.dump(trimmed_training, f, ensure_ascii=False, indent=2)
+    with open("test.json", "w") as f:
+        # Write the list of dictionaries to the file as JSON
+        json.dump(test, f, ensure_ascii=False, indent=2)
