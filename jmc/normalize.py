bdce76c0c4018372291090e516e498efa6fdd2ff
Steve Kaplan <steve@sefaria.org>
2023-02-08 11:38:03 +0200

    fix: stop tracking jmc

--- jmc/normalize.py
+++
@@ -1,8 +0,0 @@
-from util.library_exporter import TextWalker
-
-
-tw = TextWalker(open("corpus/all_text_en.txt", 'w'), open("corpus/all_text_en.jsonl", 'w'), "en")
-
-with open("all_text_en.txt") as f:
-    for line in f:
-        tw.write_lines(line.strip())
