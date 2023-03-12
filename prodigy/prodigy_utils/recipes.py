433137abbed99a435ed44b4005009f5389bc34e8
nsantacruz <noahssantacruz@gmail.com>
2023-03-12 15:08:31 +0200

    chore: increase length of texts allowed to show in prodigy

--- prodigy/prodigy_utils/recipes.py
+++ prodigy/prodigy_utils/recipes.py
@@ -148,7 +148,7 @@ def ref_tagging_recipe(dataset, input_collection, output_collection, labels, mod
     all_data = list(getattr(my_db.db, input_collection).find({}, {"_id": 0}))  # TODO loading all data into ram to avoid issues of cursor timing out
     stream = filter_existing_in_output(all_data, my_db)
     # stream = split_sentences_nltk(stream)
-    stream = filter_long_texts(stream, max_length=2000)
+    stream = filter_long_texts(stream, max_length=5000)
     if model_exists and should_add_predictions == 1:
         stream = add_model_predictions(nlp, stream, min_found=1)
     stream = add_tokens(nlp, stream, skip=True)
