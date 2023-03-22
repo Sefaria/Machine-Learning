5b4a80797a8930c5498e90f20e74c9ed0f9e2481
Noah <noahssantacruz@gmail.com>
2023-03-22 10:25:18 +0200

    feat: add param include_rejected

--- prodigy_scripts/functions.py
+++ prodigy_scripts/functions.py
@@ -7,19 +7,20 @@ from typing import Callable, Iterator, List
 import spacy
 from spacy.training import Example
 from spacy.language import Language
-from util.training_utils import get_mongo_docs, get_train_test_data, get_corpus_data
+from util.training_utils import get_corpus_data
 
 
 @spacy.registry.readers("mongo_reader")
-def stream_data(*args, **kwargs) -> Callable[[Language], Iterator[Example]]:
+def stream_data(*args, include_reject=False, **kwargs) -> Callable[[Language], Iterator[Example]]:
     corpus_data = get_corpus_data(*args, **kwargs)
 
     def generate_stream(nlp):
         for raw_example in corpus_data:
-            if raw_example['answer'] == 'reject': continue
+            answer = raw_example.get('answer', 'accept')
+            if answer == 'reject' and not include_reject: continue
             doc = nlp.make_doc(raw_example['text'])
             doc.user_data = raw_example['meta']
-            doc.user_data.update({'answer': raw_example['answer'], '_id': str(raw_example['_id'])})
+            doc.user_data.update({'answer': answer, '_id': str(raw_example['_id'])})
             entities = [(span['start'], span['end'], span['label']) for span in raw_example['spans']]
             example = Example.from_dict(doc, {"entities": entities})
             yield example
