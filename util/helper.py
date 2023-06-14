5c0a2fb54def9f9533b5222fb5691f6f14fdb2d7
nsantacruz <noahssantacruz@gmail.com>
2023-06-14 10:51:11 +0300

    feat: add get_mongo_docs_or_load_json function to encapsulate common logic

--- util/helper.py
+++ util/helper.py
@@ -1,7 +1,9 @@
 from typing import List
+import json
 from util.spacy_registry import inner_punct_tokenizer_factory
 from spacy.lang.en import English
 from spacy.lang.he import Hebrew
+
 
 def get_mongo_docs(min_training_text_len, unique_by_metadata=True, *db_manager_args) -> List[dict]:
     from db_manager import MongoProdigyDBManager
@@ -12,6 +14,15 @@ def get_mongo_docs(min_training_text_len, unique_by_metadata=True, *db_manager_a
     if unique_by_metadata:
         data = list({(tuple(sorted(d['meta'].items(), key=lambda x: x[0])), d['text']): d for d in data}.values())
     return data
+
+
+def get_mongo_docs_or_load_json(input, input_type: str, min_training_text_len, unique_by_metadata=True, *db_manager_args):
+    if input_type == "mongo":
+        return get_mongo_docs(min_training_text_len, unique_by_metadata, input, *db_manager_args)
+    elif input_type == "json":
+        with open(input, "r") as fin:
+            return json.load(fin)
+
 
 def create_normalizer():
     from sefaria.helper.normalization import NormalizerByLang, NormalizerComposer
