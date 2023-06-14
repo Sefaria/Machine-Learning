7cf457a09df4e5b3a309ab348ea66d046462eb94
nsantacruz <noahssantacruz@gmail.com>
2023-06-14 13:59:47 +0300

    feat: add ability to remove rejected docs from get_corpus_data

--- util/training_utils.py
+++ util/training_utils.py
@@ -1,14 +1,17 @@
 from typing import List
-from helper import load_mongo_docs
+from util.helper import load_mongo_docs, filter_rejected_docs
 from sklearn.model_selection import train_test_split
+
 
 def get_train_test_data(random_state: int, mongo_docs: List[dict], training_percentage: float):
     train_data, test_data = train_test_split(mongo_docs, random_state=random_state, train_size=training_percentage)
     return train_data, test_data
 
 
-def get_corpus_data(db_host: str, db_port: int, input_collection: str, random_state: int, train_perc: float, corpus_type: str, min_len: int, unique_by_metadata=True):
+def get_corpus_data(db_host: str, db_port: int, input_collection: str, random_state: int, train_perc: float, corpus_type: str, min_len: int, unique_by_metadata=True, include_reject=False):
     data = load_mongo_docs(min_len, unique_by_metadata, input_collection, db_host, db_port)
+    if not include_reject:
+        data = filter_rejected_docs(data)
     print("Num examples", len(data))
     if random_state == -1:
         train_data, test_data = (data, []) if corpus_type == "train" else ([], data)
