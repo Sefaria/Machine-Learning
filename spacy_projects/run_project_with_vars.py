368dadf51ace2ca9f64d73c0c6b7224b955059f7
Noah <noahssantacruz@gmail.com>
2024-10-16 05:50:20 +0300

    chore: update path

--- spacy_projects/run_project_with_vars.py
+++ spacy_projects/run_project_with_vars.py
@@ -4,7 +4,7 @@ import yaml
 
 
 def run_project_with_vars(project: str, vars_name: str, task: str):
-    with open(f"../{project}/vars/{vars_name}.yml", 'r') as fin:
+    with open(f"./{project}/vars/{vars_name}.yml", 'r') as fin:
         yin = yaml.safe_load(fin)
         cmd = f"python -m spacy project run {task} .".split()
         print_out_cmd = cmd
@@ -13,7 +13,7 @@ def run_project_with_vars(project: str, vars_name: str, task: str):
             if "password" not in var_name:
                 print_out_cmd += [f'--vars.{var_name}', f"{var_value}"]
         print("Running command:", print_out_cmd)
-        process = subprocess.Popen(cmd, cwd=f"../{project}")
+        process = subprocess.Popen(cmd, cwd=f"./{project}")
         stdout, stderr = process.communicate()
 
 
